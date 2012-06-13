5a1a908d13543b8a7193772f08c941908299f8c8
Hans <hansmaesalu@gmail.com>
2012-06-14 00:14:46 +0300

    RuleML service based on OOjDREW engine.

---
+++ RuleMLApp/src/java/ee/ut/hans/RuleML/service/messages/Request.java
@@ -0,0 +1,29 @@
+package ee.ut.hans.RuleML.service.messages;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Hans
+ */
+public class Request implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String ruleset;
+
+	/**
+	 * @return the mathml
+	 */
+	public String getRuleset() {
+		return ruleset;
+	}
+
+	/**
+	 * @param mathml the mathml to set
+	 */
+	public void setRuleset(String ruleset) {
+		this.ruleset = ruleset;
+	}
+	
+}
